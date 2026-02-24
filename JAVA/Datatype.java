c412ef3aed6ecddcaf62993f437731ae4925f4fe
sanjana kumar <sanjana14kumar07@gmail.com>
2026-02-24 23:49:15 +0530

    Added Datatype.java

---
+++ JAVA/Datatype.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Datatype {
+    public static void main(String[] args) {
+      int a=5;
+      int b;
+      b=++a;
+      System.out.println(a);
+      System.out.println(b);
+    }
+}
